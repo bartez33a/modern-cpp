e1075d5a1b36aed64a3c463b130d05ecab6c379a
Bartosz Rozenkop <bartez33a@gmail.com>
2023-10-18 10:48:04 +0200

    Basic features - nullptr
    - nullptr casting/conversion

--- 01-basic-features/exercises/main.cpp
+++ 01-basic-features/exercises/main.cpp
@@ -6,5 +6,16 @@ int main() {
 
   std::cout << "version of C++ " << __cplusplus << '\n';
 
+  // nullptr (std::nullptr_t)
+  int x = NULL;
+
+  // nullptr cannot be implicitly converted to int
+  // int y = nullptr; // error
+
+  // possible - C-style casting should not be perfomed!
+  int z = (long long)nullptr;
+
+  int zz = reinterpret_cast<long long>(nullptr);
+
   return 0;
 }
